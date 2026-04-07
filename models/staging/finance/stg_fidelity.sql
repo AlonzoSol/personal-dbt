f0ab55b498817e0df605608d236c99949b83bfc6
Alonzo <solorzano.alonzo@gmail.com>
2026-04-07 04:58:44 +0000

    pass key fields

--- models/staging/finance/stg_fidelity.sql
+++ models/staging/finance/stg_fidelity.sql
@@ -1,4 +1,15 @@
 with source as (
     select * from {{ ref('fidelity') }}
+),
+
+final as (
+    select
+        "Run Date" as date,
+        "Action" as transaction,
+        "Amount ($)" as amount,
+        "Cash Balance ($)" as balance
+    
+    from source
 )
 
+select * from final
