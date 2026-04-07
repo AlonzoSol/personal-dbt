9b91ca574016479a6f0cdd7db393fe098543b94b
Alonzo <solorzano.alonzo@gmail.com>
2026-04-07 05:19:26 +0000

    extract columns

--- models/staging/finance/stg_chase.sql
+++ models/staging/finance/stg_chase.sql
@@ -1,3 +1,18 @@
 with source as (
     select * from {{ ref('chase') }}
+),
+
+final as (
+    select
+        "Transaction Date" as transaction_date,
+        "Post Date" as post_date,
+        "Description" as description,
+        "Category" as category,
+        "Type" as type,
+        "Amount" as amount,
+        "Memo" as memo
+        
+    from source
 )
+
+select * from final
