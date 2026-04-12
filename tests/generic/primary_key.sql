42a719cc4064d1510d17131b7f2f05742348c3e0
Alonzo <solorzano.alonzo@gmail.com>
2026-04-12 13:49:49 +0000

    remove cte

--- tests/generic/primary_key.sql
+++ tests/generic/primary_key.sql
@@ -19,15 +19,10 @@ unique_errors as (
     where primary_key is not null
     group by primary_key
     having count(*) > 1
-),
-
-joined_errors as (
-	select primary_key
-	from null_errors
-		full join unique_errors using(primary_key) 
 )
 
-select *
-from joined_errors
+select primary_key
+from null_errors
+	full join unique_errors using(primary_key)
 
 {% endtest %}
