3305129cdd5f20185fa1217eecf61dace0cf37b4
Alonzo <solorzano.alonzo@gmail.com>
2026-04-12 12:49:08 +0000

    generate primary key

--- models/staging/finance/stg_chase.sql
+++ models/staging/finance/stg_chase.sql
@@ -2,7 +2,7 @@ with source as (
     select * from {{ ref('chase') }}
 ),
 
-final as (
+typed as (
     select
         "Transaction Date"::date as transaction_date,
         "Post Date"::date as post_date,
@@ -13,6 +13,13 @@ final as (
         "Memo" as memo
 
     from source
+),
+
+final as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['transaction_date', 'description', 'amount']) }} as primary_key,
+        *
+    from typed
 )
 
 select * from final
